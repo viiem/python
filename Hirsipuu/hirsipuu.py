fc0cb8b04b170922de35cfd926266d1cb6dd1cc0
viiem <viitanenem@gmail.com>
2025-05-05 15:13:09 +0300

    Update hirsipuu.py

--- Hirsipuu/hirsipuu.py
+++ Hirsipuu/hirsipuu.py
@@ -1,7 +1,17 @@
 """
 Hirsipuu-peli
 Arpoo sanojen joukosta jonkun sanan ja pelaajan tulee arvata se sana
+#if input.lower == "b" or input.lower == "luonto":""
+#if input.lower == "c" or input.lower == "satunnainen":"
 """
 sanalistaRuoka = '''kinuskipehmis perunakellari viinirypäle porkkanalaatikko'''
-sanalistaLuonto = '''kuparimalmi pajunkissa lumpeenlehti vaippapaviaani '''
+sanalistaLuonto = '''kuparimalmi pajunkissa lumpeenlehti vaippapaviaani'''
 sanalistaSatunnainen = '''sinkkunainen elämänkumppani alkoholinkäyttö pyykinpesupäivä'''
+
+import random
+from collections import Counter
+
+while True:
+    input = input("Valitse kategoria: (A) Ruoka (B) Luonto (C) Satunnainen")
+
+    if input.lower == "a" or input.lower == "ruoka":
