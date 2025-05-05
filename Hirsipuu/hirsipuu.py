6f5651df7bd372d155c7433358bf1fab4f8035eb
viiem <viitanenem@gmail.com>
2025-05-05 15:07:43 +0300

    Create hirsipuu.py

---
+++ Hirsipuu/hirsipuu.py
@@ -0,0 +1,7 @@
+"""
+Hirsipuu-peli
+Arpoo sanojen joukosta jonkun sanan ja pelaajan tulee arvata se sana
+"""
+sanalistaRuoka = '''kinuskipehmis perunakellari viinirypäle porkkanalaatikko'''
+sanalistaLuonto = '''kuparimalmi pajunkissa lumpeenlehti vaippapaviaani '''
+sanalistaSatunnainen = '''sinukkunainen elämänkumppani alkoholinkäyttö pyykinpesupäivä'''
