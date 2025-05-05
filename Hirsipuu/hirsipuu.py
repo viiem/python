f8201e998cba229039f08e076c79ec77692f4dd4
viiem <viitanenem@gmail.com>
2025-05-05 15:37:24 +0300

    Update hirsipuu.py

--- Hirsipuu/hirsipuu.py
+++ Hirsipuu/hirsipuu.py
@@ -11,7 +11,36 @@ sanalistaSatunnainen = '''sinkkunainen elämänkumppani alkoholinkäyttö pyykin
 import random
 from collections import Counter
 
+print("Pelataan Hirsipuuta!")
+
+saannot = input("Haluatko lukea säännöt ennen pelaamista?")
+if saannot.lower == "kyllä":
+    print("Tässä säännöt: Saat kolme mahdollisuutta.")
+    print()
+    print("Nyt pelataan!")
+if saannot.lower == "ei":
+    print("Nyt pelataan!")
+else:
+    print("Vastaa kyllä tai ei.")
+
 while True:
     input = input("Valitse kategoria: (A) Ruoka (B) Luonto (C) Satunnainen")
 
     if input.lower == "a" or input.lower == "ruoka":
+        sanalistaRuoka = sanalistaRuoka.split(' ')
+        sana = random.choice(sanalistaRuoka)
+
+        print("Arvaa sana! Sana liittyy aiheeseen ruoka")
+
+        for i in sana:
+            print('_', end=' ')
+        print()
+        pelataan = True
+        arvattu = ''
+        elamat = 3
+        oikein = 0
+        jatketaanko = 0
+        try:
+            while (elamat != 0) and jatketaanko == 0:
+                print()
+                elamat
